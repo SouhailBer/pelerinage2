0ffac3fc26f9c708fc46a5af607a9562aee53c6a
SouhailBer <bersouhail@gmail.com>
2020-12-11 22:54:23 +0100

    pelerinage2:1st commit

---
+++ js_peleri.js
@@ -0,0 +1,22 @@
+function display1() {
+	if (document.getElementById("rdh").checked==true) {
+		document.getElementById("div2").style.display="none";		
+	}
+	
+else
+		document.getElementById("div2").style.display="block";
+		
+}
+
+function display2() {
+	if (document.getElementById("rdh").checked==true) {
+        document.getElementById("pmahram").style.display="none";
+		
+	}
+	else
+		document.getElementById("pmahram").style.display="block";
+		
+	
+
+}
+
